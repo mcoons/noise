73c7a6b22b3a9202de4a0d912766ad2d34f9c6de
mcoons <mcoons67@gmail.com>
2021-03-20 00:45:26 -0600

    adjusting size

--- index.js
+++ index.js
@@ -22,6 +22,50 @@ window.onload = function () {
 
         scene.activeCamera.attachControl(canvas);
 
+
+
+
+        scene.onPointerObservable.add((pointerInfo) => {
+            switch (pointerInfo.type) {
+                case BABYLON.PointerEventTypes.POINTERDOWN:
+                    console.log("POINTER DOWN");
+                    break;
+                case BABYLON.PointerEventTypes.POINTERUP:
+                    console.log("POINTER UP");
+                    break;
+                case BABYLON.PointerEventTypes.POINTERMOVE:
+                    console.log("POINTER MOVE");
+                    break;
+                case BABYLON.PointerEventTypes.POINTERWHEEL:
+                    console.log("POINTER WHEEL");
+                    break;
+                case BABYLON.PointerEventTypes.POINTERPICK:
+                    console.log("POINTER PICK");
+                    console.log(pointerInfo);
+                    break;
+                case BABYLON.PointerEventTypes.POINTERTAP:
+                    console.log("POINTER TAP");
+                    break;
+                case BABYLON.PointerEventTypes.POINTERDOUBLETAP:
+                    console.log("POINTER DOUBLE-TAP");
+                    break;
+            }
+        });
+    
+        scene.onKeyboardObservable.add((kbInfo) => {
+            switch (kbInfo.type) {
+                case BABYLON.KeyboardEventTypes.KEYDOWN:
+                    console.log("KEY DOWN: ", kbInfo.event.key);
+                    console.log(kbInfo);
+                    break;
+                case BABYLON.KeyboardEventTypes.KEYUP:
+                    console.log("KEY UP: ", kbInfo.event.keyCode);
+                    break;
+            }
+        });
+
+        
+
         engine.runRenderLoop(function () {
             drawTexture(noiseFunctions[functionNumber]);
             scene.render();
